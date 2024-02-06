4f2bc472954d309be812ee2dbf2825a703541ad5
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-02-06 11:47:02 +0200

    Adding binary search function by recursive

--- search_algorithms/binary.c
+++ search_algorithms/binary.c
@@ -60,7 +60,10 @@ int binary_search_recursive(int *array, size_t size, int value)
 
 	size_t mid = size / 2;
 
-	/*         (array, left, right);   */
+	/*  left = 0, right = size - 1
+		mid = left + (right - left) / 2
+		passing will be like that => (array, left, right);
+	*/
 	print_array(array, 0, size - 1);
 	if (array[mid] == value)
 		return (mid);
